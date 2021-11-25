566f395537649369a73d6839a8c6f819001ac05f
Carlos <capz2003@gmail.com>
2021-11-25 12:25:15 -0500

    switch figures

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import images, { data } from "./images";
 
-function App() {
+export default function App() {
+  const imagesList = data;
+  const [currentList, setCurrentList] = useState();
+  const [historicList, setHistoricList] = useState([]);
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    generateRandomList();
+  }, []);
+
+  useEffect(() => {
+    if (counter === 5) addHistoric();
+  }, [counter]);
+
+  function addHistoric() {
+    setTimeout(() => {
+      setHistoricList(historicList.concat(currentList));
+      generateRandomList();
+      setCounter(0);
+    }, 1000);
+  }
+
+  function generateRandomList() {
+    let newList = {
+      isComplete: false,
+      data: [],
+    };
+    for (let index = 0; index < 5; index++) {
+      const randomIndex = Math.floor(Math.random() * (9 - 0 + 1));
+      let newObj = {
+        id: imagesList[randomIndex].name + index,
+        url: imagesList[randomIndex].url,
+        isVisible: false,
+      };
+      newList.data.push(newObj);
+    }
+    setCurrentList(newList);
+  }
+
+  const handleClick = (id) => {
+    
+    const newData = currentList.data.map((item) => {
+      if (item.id === id) item.isVisible = true;
+      
+      if (item.isVisible) setCounter(counter + 1);
+      return item;
+    });
+    setCurrentList({ ...currentList, data: newData });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1> Pokemon </h1>
+      <div className="rowup">
+        {currentList
+          ? currentList.data.map((item) => (
+              <div key={item.id} className="col">
+                {item.isVisible ? (
+                  <img src={item.url} alt={item.id} className="image" />
+                ) : (
+                  <div
+                    className="square"
+                    onClick={() => handleClick(item.id)}
+                  ></div>
+                )}
+              </div>
+            ))
+          : null}
+      </div>
+      {historicList.map((item, index) => (
+        <div key={index} className="rowup">
+          {item.data.map((image) => (
+            <div key={image.id} className="col">
+              <img src={image.url} alt={image.id} className="image" />
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
-
-export default App;
---
+++ src/images.js
@@ -0,0 +1,53 @@
+module.exports = {
+  data: [
+    {
+      name: "Gengar",
+      url:
+        "https://i.pinimg.com/originals/cb/87/f1/cb87f16d5dcf7c0fcf8b6b8c9ccf8f14.jpg"
+    },
+    {
+      name: "Squirtle",
+      url:
+        "https://key0.cc/images/preview/46345_f21eaee2e43caf3ddbaaacc7c3257c7f.png"
+    },
+    {
+      name: "Bulbasaur",
+      url:
+        "https://img2.freepng.es/20190516/hfu/kisspng-bulbasaur-squirtle-ivysaur-charmander-venusaur-file-1bulbasaur-ag-anime-png-bulbagarden-archi-5cde2f9cc60599.7660216015580650528111.jpg"
+    },
+    {
+      name: "Charmander",
+      url:
+        "https://w7.pngwing.com/pngs/156/686/png-transparent-pokemon-go-pokemon-x-and-y-ash-ketchum-charmander-pokemon-background-orange-cartoon-fictional-character.png"
+    },
+    {
+      name: "Pikachu",
+      url: "http://assets.stickpng.com/images/580b57fcd9996e24bc43c325.png"
+    },
+    {
+      name: "Mewtwo",
+      url:
+        "https://w7.pngwing.com/pngs/697/873/png-transparent-pokemon-red-and-blue-pokemon-x-and-y-pokemon-go-mewtwo-pokemon-go-purple-mammal-cat-like-mammal-thumbnail.png"
+    },
+    {
+      name: "Mew",
+      url:
+        "https://i.pinimg.com/originals/5f/14/cd/5f14cde7aab7f459cb6d31a8aa44de47.png"
+    },
+    {
+      name: "Vaporeon",
+      url:
+        "https://w7.pngwing.com/pngs/863/210/png-transparent-pokemon-go-pikachu-vaporeon-eevee-pokemon-go-dragon-fictional-character-cartoon.png"
+    },
+    {
+      name: "Flareon",
+      url:
+        "https://e7.pngegg.com/pngimages/163/482/png-clipart-pokemon-go-pokemon-channel-eevee-flareon-pokemon-go-mammal-carnivoran.png"
+    },
+    {
+      name: "Jolteon",
+      url:
+        "https://e7.pngegg.com/pngimages/88/871/png-clipart-pokemon-yellow-pokemon-x-and-y-jolteon-eevee-pikachu-pikachu-mammal-carnivoran.png"
+    }
+  ]
+}
